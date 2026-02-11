dab3ef60029320e5c9a4a277be1f3d887377a885
LayaSync <layasync@gmail.com>
2026-02-11 23:32:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ document.addEventListener("DOMContentLoaded", function(){
       const row = document.createElement("div");
       row.className="row";
 
-      row.onclick = ()=> loadMovies(cat.category_id, row);
+      loadMovies(cat.category_id, row);
       section.appendChild(title);
       section.appendChild(row);
       container.appendChild(section);
